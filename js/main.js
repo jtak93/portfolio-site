77716c3742a9720d0b30c8b1878b805a1c3268d2
james tak <jtak93@gmail.com>
2016-11-11 16:21:43 -0800

    portfolio page completed

--- js/main.js
+++ js/main.js
@@ -4,34 +4,98 @@ $(document).ready(function() {
   var $template = $("#template");
   homeTemplate = $('#home').html()
   $template.html(homeTemplate)
+  // initiate sidenav
   $('.button-collapse').sideNav({
-      menuWidth: 300,
+      menuWidth: 200,
       closeOnClick: true
     }
   );
 
-  $('.about').on('click', () => {
-    var template = $('#about').html();
-    console.log(template)
-    console.log("clicked")
-    $template.html(template);
-    $('.parallax').parallax();
-    $('.carousel').carousel();
-  })
+  // Animations
+  welcomeMsgAnim();
 
-  $('.portfolio').on('click', () => {
-    console.log('clicked!')
-    var template = $('#portfolio').html();
-    $template.html(template);
-    $('.carousel.carousel-slider').carousel({full_width: true});
-    console.log(template)
-  })
+  function welcomeMsgAnim() {
+    $('#welcomeMsg').css({"visibility":"visible"});
+    $('#welcomeMsg').addClass('animated fadeInUp');
+    $('#welcomeMsg').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+      $('#welcomeMsg2').css({"visibility":"visible"});
+      $('#welcomeMsg2').addClass('animated fadeInUp')
+      $('#welcomeMsg2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+        $('#welcomeMsg3').css({"visibility":"visible"});
+        $('#welcomeMsg3').addClass('animated fadeInUp');
+        $('#welcomeMsg3').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
+          $('#welcomeMsgBtn').css({"visibility":"visible"});
+          $('#welcomeMsgBtn').addClass('animated fadeIn');
+          addAboutEvent();
+        });
+      });
+    });
+  }
 
-  $('.contact').on('click', () => {
-    console.log('clicked!')
-    var template = $('#contact').html();
-    $template.html(template);
-    console.log(template)
-  })
+
+  // Events
+  addAboutEvent()
+  addPortfolioEvent()
+  addContactEvent()
+  addBrandEvent()
+  function addELoungeInfoEvent() {
+    $('.e-lounge-info-btn').on('click', () => {
+      var template = $('#e-lounge-info').html();
+      var $infoTemplate = $('.project-desc')
+      $infoTemplate.html(template);
+    })
+  }
+
+  function addBBInfoEvent() {
+    $('.bbc-info-btn').on('click', () => {
+      var template = $('#bbc-info').html();
+      var $infoTemplate = $('.project-desc')
+      $infoTemplate.html(template);
+    })
+  }
+
+  function addLarkInfoEvent() {
+    $('.lark-info-btn').on('click', () => {
+      var template = $('#lark-info').html();
+      var $infoTemplate = $('.project-desc')
+      $infoTemplate.html(template);
+    })
+  }
+
+  function addAboutEvent() {
+    $('.about').on('click', () => {
+      var template = $('#about').html();
+      $template.html(template);
+      $('.parallax').parallax();
+      $('.carousel').carousel();
+      addPortfolioEvent()
+    })
+  }
+
+  function addPortfolioEvent() {
+    $('.portfolio').on('click', () => {
+      var template = $('#portfolio').html();
+      $template.html(template);
+      $('.carousel.carousel-slider').carousel({full_width: true});
+      addELoungeInfoEvent();
+      addBBInfoEvent();
+      addLarkInfoEvent();
+    })
+  }
+
+  function addContactEvent() {
+    $('.contact').on('click', () => {
+      var template = $('#contact').html();
+      $template.html(template);
+    })
+  }
+
+  function addBrandEvent() {
+    $('.brand-logo').on('click', () => {
+      var template = $('#home').html();
+      $template.html(template);
+      welcomeMsgAnim();
+    })
+  }
 
 });
